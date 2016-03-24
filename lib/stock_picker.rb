904aa5ca1ebaa673e908b983960c1797bc23c4e9
Kate Flood <kathryneflood@gmail.com>
2016-03-23 19:16:39 -0500

    updated rspec

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,19 +1,51 @@
 class StockPicker
 
-#stock_array = ""
+low_price = 0
+high_price = 0
+max_profit = 0
 
 def pick(stock_array)
   stock_prices = stock_array.sort
   low_price = stock_prices[0]
   high_price = stock_prices[-1]
-  best_price = stock_prices[-1] - stock_prices[0]
-  if stock_array.index(low_price) < stock_array.index(high_price)
-    [low_price, high_price]
-  end
+    if stock_array.index(low_price) < stock_array.index(high_price)
+      [stock_array.index(low_price), stock_array.index(high_price)]
+    else
+      difference = stock_array.map { |stocks| stock_prices[-1] - stocks while stock_array.index(stock_prices[-1]) > stock_array.index(stocks) }
+      #picker = difference.max
+
+#  stock_prices = stock_array.minmax
+#  low_price = stock_prices[0]
+#  high_price = stock_prices[-1]
+#  if stock_array.index(low_price) < stock_array.index(high_price)
+#    [low_price, high_price]
+#  else
+#    biggest_difference(stock_array)
+#    end
 end
+  end
+
+  #stock_prices = stock_array.sort
+  #low_price = stock_prices[0]
+  #high_price = stock_prices[-1]
+  #best_price = stock_prices[-1] - stock_prices[0]
+  #if stock_array.index(low_price) < stock_array.index(high_price)
+  #  [low_price, high_price]
+  #else
+
+  #end
 
 private
 
+def biggest_difference(stock_array)
+  sorted = stock_array.sort
+  highest = sorted[-1]
+  stock_array.each_with_index do |price, idx|
+    puts [price, idx]
+  #new_sort = sorted.map { |x| highest - x }
+  #new_sort.delete_at(-1)
+  #new_sort
+end
 #def lowest(stock_array)
 #  stock_prices = stock_array.sort
 #  low_price = stock_prices[0]
@@ -27,4 +59,4 @@ private
 #end
 
 end
-#a.index("b")
+end
--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -4,8 +4,21 @@ require 'stock_picker'
 describe StockPicker do
   let(:stock_picker) {StockPicker.new}
 
-  it "can pick stocks from a set of two" do
-    expect(stock_picker.pick([1,2])).to eq [1, 2]
+  it "can pick stocks from a set of two in the right order" do
+    expect(stock_picker.pick([1,2])).to eq [0, 1]
   end
+
+  it "can pick stocks from a set of three in the right order" do
+    expect(stock_picker.pick([1,2,3])).to eq [0, 2]
+  end
+
+  it "can pick stocks from a set of three from a mixed array" do
+    expect(stock_picker.pick([1, 9, 5])).to eq [0, 1]
+  end
+
+  it "can pick stocks when the lowest price is after the highest" do
+    expect(stock_picker.pick([8, 2, 6])).to eq [1, 2]
+  end
+
 
 end
